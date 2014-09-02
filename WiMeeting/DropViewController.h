994ccdd922ddc863d86f22bd52fdadfd0b204dbf
hc_lo <hc_lo@wistron.com>
2014-09-02 10:22:17 +0800

    - DropViewController Testing

---
+++ WiMeeting/DropViewController.h
@@ -0,0 +1,13 @@
+//
+//  DropViewController.h
+//  WiMeeting
+//
+//  Created by HungChe Lo on 9/2/14.
+//  Copyright (c) 2014 Wistron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropViewController : UIViewController
+
+@end
